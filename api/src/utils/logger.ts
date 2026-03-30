733becc756b528204054330151297ee3622ba662
sublime247 <kushimobashir2000@gmail.com>
2026-03-30 10:32:18 +0100

    feat(request-id-middleware)

--- api/src/utils/logger.ts
+++ api/src/utils/logger.ts
@@ -1,9 +1,21 @@
 import winston from 'winston';
+import { AsyncLocalStorage } from 'async_hooks';
 import { config } from '../config';
+
+export const requestContext = new AsyncLocalStorage<string>();
+
+const addRequestId = winston.format((info: winston.Logform.TransformableInfo) => {
+  const reqId = requestContext.getStore();
+  if (reqId) {
+    info.requestId = reqId;
+  }
+  return info;
+});
 
 const logger = winston.createLogger({
   level: config.logging.level,
   format: winston.format.combine(
+    addRequestId(),
     winston.format.timestamp(),
     winston.format.errors({ stack: true }),
     winston.format.json()
